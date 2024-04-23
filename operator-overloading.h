0fdab3dc58d077ee9b991a04ddf9216aaa0d4be3
Hughhc <2106094894@qq.com>
2024-04-23 21:38:42 +0800

    重载左移运算符

--- operator-overloading.h
+++ operator-overloading.h
@@ -9,15 +9,20 @@ namespace operator_overloading
 	class CGirl
 	{
 		// 友元，函数
-		friend CGirl& operator+(CGirl& g, int score);
-		friend CGirl& operator-(CGirl& g, int score);
-		friend CGirl& operator+(int score, CGirl& g);
-		friend CGirl& operator+(CGirl& g1, CGirl& g2);
+		//friend CGirl& operator+(CGirl& g, int score);
+		//friend CGirl& operator-(CGirl& g, int score);
+		//friend CGirl& operator+(int score, CGirl& g);
+		//friend CGirl& operator+(CGirl& g1, CGirl& g2);
+
+		friend ostream& operator<<(ostream& cout, const CGirl& g);
 	private:
+		// 左移运算符重载
+		string m_name;  // 姓名
+
 		int m_xw;		//胸围
 		int m_score;	//分数
 	public:
-		string m_name;  // 姓名
+		//string m_name;  // 姓名
 
 		//默认构造函数
 		CGirl();
@@ -29,11 +34,13 @@ namespace operator_overloading
 	};
 
 	// 重载运算符 非成员函数重载
-	CGirl& operator+(CGirl& g, int score);
-	CGirl& operator-(CGirl& g, int score);
-	// 重载函数参数列表中的顺序决定了操作数的位置
-	CGirl& operator+(int score, CGirl& g);
-	CGirl& operator+(CGirl& g1, CGirl& g2);
+	//CGirl& operator+(CGirl& g, int score);
+	//CGirl& operator-(CGirl& g, int score);
+	//// 重载函数参数列表中的顺序决定了操作数的位置
+	//CGirl& operator+(int score, CGirl& g);
+	//CGirl& operator+(CGirl& g1, CGirl& g2);
+
+	ostream& operator<<(ostream& cout, const CGirl& g);
 
 	void printCGirl();
 }
